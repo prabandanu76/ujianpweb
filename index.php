072e41d86ed337065581fed702321fb18367afb7
prabandanu76 <prabandanu76@gmail.com>
2023-12-04 16:36:24 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+include 'functions.php';
+// Your PHP code here.
+
+// Home Page template below.
+?>
+
+<?=template_header('Home')?>
+
+<div class="content">
+	<h2>Home</h2>
+	<p>Selamat Datang Di Web Pendataan Karyawan!</p>
+	<P>untuk Mengisi data harap membuka menu Contact</P>
+</div>
+
+<?=template_footer()?>
